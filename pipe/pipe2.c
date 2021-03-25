72d519b375cd82f7f91d4da5b2d5f146d221ef29
jOKERII <631854721@qq.com>
2021-03-25 23:14:13 +0800

    start pipe

---
+++ pipe/pipe2.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <unistd.h>
+
+#define BUF_SIZE 30
+
+
+int main(int argc, char *argv[])
+{
+	int fds[2];
+	char str1[] = "test";
+	char str2[] = "getted";
+	char buf[BUF_SIZE];
+	pid_t pid;
+	
+	pipe(fds);
+	pid = fork();
+	
+	if(pid == 0)
+	{
+		write(fds[1], str1, sizeof(str1));
+		sleep(2);
+		read(fds[0], buf, BUF_SIZE);
+		printf("child proc output: %s \n", buf);
+
+	}
+	else
+	{
+		read(fds[0], buf, BUF_SIZE);
+		printf("Parent proc output: %s \n",buf);
+		write(fds[1], str2, sizeof(str2));
+		sleep(3);
+	}
+	
+	return 0;
+}
+
